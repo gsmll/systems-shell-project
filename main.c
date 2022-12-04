0bf881db88b6526ba657f9811af477cb96c3dae7
Gabriel Small <gsmall30@stuy.edu>
2022-12-04 11:28:31 -0800

    added semicolon parsing fixed space errors

--- main.c
+++ main.c
@@ -1,11 +1,27 @@
 #include "parse.h"
 
 // executefunction:
-// returns the function return value or errno on error
+// returns the function return value or errno on error or 0 on success
 // params:  args [ pointer to args ] 
 // executes function in given args
 
-int executefunction(char** args){
+int executefunction(char** args, int argc){
+
+  // tests for exit or cd
+   if(strcmp(args[0],"exit") == 0 )
+       exit(argc == 2 ? atoi(args[1]) : 0);
+   if(strcmp(args[0],"cd") == 0 ){
+      if(argc == 2){
+      chdir(args[1]);
+      return 0;
+      }
+      else{
+      printf("too many args [cd]\n");
+      return -1;
+      }
+    }
+
+  // runs seperate programs
   int id = fork();
   
   if(id != 0){
@@ -28,47 +44,27 @@ int executefunction(char** args){
   execvp(args[0], args);
   return errno;
 }
-
+// runs execute for each function seperated by a semicolon, takes in stdin for now
+void executeargs(char* input){
+    char* sect;
+    char* search;
+    search = strdup(input);
+    while((sect = strsep(&search,";")) != NULL){
+      char argc;
+      char** args = parse_args(sect, &argc);
+      executefunction(args, argc);
+    }
+    return;
+}
 int main() {
   while(1){
     char cwd [1024];
     getcwd(cwd, sizeof(cwd));
     printf("[%s] shell@%s $ ",cwd,getpwuid(geteuid())->pw_name);
-    char buffer[100] ;
-    fgets(buffer, 100, stdin);
+    char buffer[1024] ;
+    fgets(buffer, 1024, stdin);
     buffer[strlen(buffer)-1] =0;
-    char argc;
-    char** args = parse_args(buffer, &argc);
-    if(strcmp(args[0],"exit") == 0 )
-       exit(argc == 1 ? atoi(args[1]) : 0);
-    if(strcmp(args[0],"cd") == 0 ){
-      if(argc == 1)
-      chdir(args[1]);
-      else
-      printf("too many args [cd]\n");
-    }
-    else executefunction(args);
-    
-
-    
-    
-    
+    executeargs(buffer);    
   }
-  // int id, id2;
-  // (id = fork()) && (id2 = fork());
-
-  // if (id != 0 && id2 != 0) {
-  //   int status;
-  //   wait(&status);
-  //   printf("%d finished, waited for %ds\n", id, WEXITSTATUS(status));
-  //   printf("parent done\n");
-  //   exit(0);
-  // }
-  // srand(getpid());
-  // int x = (rand() % 4) + 2;
-  // printf("%d %dsec\n", getpid(), x);
-  // sleep(x);
-  // printf("%d finished\n", getpid());
-  // exit(x);
   return 0;
 }
--- parse.c
+++ parse.c
@@ -1,23 +1,30 @@
 
 #include "parse.h"
+
+
+// parses through arguments, first counting amount of args to malloc arg array, then going enumerating each argument value to the array args.
+// takes in parameters argc [char for arg array length] and line [line to be parsed]
+// returns array of arguments from line.
 char **parse_args(char *line, char* argc) {
   char *t;
   int count = 0;
   int c = 0;
   while (*(line + c) != '\0') {
-    if (*(line + c) != ' ' && (*(line + c - 1) == ' '))
+    if (*(line + c) != ' ' && ((*(line + c - 1) == ' ') || (*(line + c - 1) == 0)))
       count++;
     c++;
   }
-  char **args = malloc(8 * (count + 2));
+  char **args = malloc(8 * (count + 1));
   c = 0;
   char *cur = line;
-  while (c != count + 1) {
+  while (c != count ) {
     t = strsep(&cur, " ");
+    if(t[0] != 0){
     args[c] = t;
     c++;
+    }
   }
-  args[count+1] = NULL;
+  args[count] = NULL;
   *argc = count;
   return args;
 }
